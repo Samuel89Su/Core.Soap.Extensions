17f56e3bd6c123bc1f16c27797442d407ec149e2
Samuel89Su <sushoucai1989@hotmail.com>
2021-08-06 14:47:16 +0800

    test proxy

--- SoapProxy.WebApiHost/ApiBrokerMiddleware.cs
+++ SoapProxy.WebApiHost/ApiBrokerMiddleware.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Services.Protocols;
 
 namespace SoapProxy.WebApiHost
 {
@@ -18,7 +19,7 @@ namespace SoapProxy.WebApiHost
         static ApiBrokerMiddleware()
         {
             _clientTypes = (_clientTypes == null || _clientTypes.Count() == 0)
-                ? Assembly.GetExecutingAssembly().GetTypes().Where(t => typeof(ICommunicationObject).IsAssignableFrom(t))
+                ? Assembly.GetExecutingAssembly().GetTypes().Where(t => typeof(ICommunicationObject).IsAssignableFrom(t) || typeof(SoapHttpClientProtocol).IsAssignableFrom(t))
                 : _clientTypes;
         }
 
@@ -29,64 +30,71 @@ namespace SoapProxy.WebApiHost
         public override async Task Invoke(IOwinContext context)
         {
             var request = context.Request;
-            if ((PathServiceMap.Maps?.Count() ?? 0) == 0)
+            try
             {
-                await this.Next.Invoke(context);
-                return;
-            }
-            var pathMap = PathServiceMap.Maps.FirstOrDefault(m => request.Path.StartsWithSegments(new PathString(m.PathRoot)));
-            if (pathMap == null)
-            {
-                await this.Next.Invoke(context);
-                return;
-            }
-            var clientType = _clientTypes.FirstOrDefault(t => t.FullName.Equals(pathMap.ServiceClient));
-            if (clientType == null)
-            {
-                await this.Next.Invoke(context);
-                return;
-            }
-
-            var actionName = pathMap.PathActions.FirstOrDefault(m => request.Path.StartsWithSegments(new PathString(m.Key))).Value;
-            MethodInfo actionMethod = null;
-            if (string.IsNullOrWhiteSpace(actionName))
-            {
-                actionMethod = clientType.GetMethod(request.Path.Value.Split('/').Last(), BindingFlags.Public);
-            }
-            else
-            {
-                actionMethod = clientType.GetMethod(actionName, BindingFlags.Public);
-            }
-            if (actionMethod == null)
-            {
-                await this.Next.Invoke(context);
-                return;
-            }
-
-            var parameters = new object[0];
-            var parameterInfos = actionMethod.GetParameters();
-            if (parameterInfos != null || parameterInfos.Count() > 0)
-            {
-                if (request.ContentType.StartsWith("application/x-www-form-urlencoded", StringComparison.OrdinalIgnoreCase))
+                if ((PathServiceMap.Maps?.Count() ?? 0) == 0)
                 {
-                    var form = await request.ReadFormAsync();
-                    parameters = ParseForm(form, parameterInfos);
+                    await this.Next.Invoke(context);
+                    return;
                 }
-                else if (request.ContentType.StartsWith("application/json", StringComparison.OrdinalIgnoreCase))
+                var pathMap = PathServiceMap.Maps.FirstOrDefault(m => request.Path.StartsWithSegments(new PathString(m.PathRoot)));
+                if (pathMap == null)
                 {
-                    using (var reader = new StreamReader(request.Body))
+                    await this.Next.Invoke(context);
+                    return;
+                }
+                var clientType = _clientTypes.FirstOrDefault(t => t.FullName.Equals(pathMap.ServiceClient));
+                if (clientType == null)
+                {
+                    await this.Next.Invoke(context);
+                    return;
+                }
+
+                var actionName = pathMap.PathActions.FirstOrDefault(m => request.Path.StartsWithSegments(new PathString(m.Key))).Value;
+                MethodInfo actionMethod = null;
+                if (string.IsNullOrWhiteSpace(actionName))
+                {
+                    actionMethod = clientType.GetMethod(request.Path.Value.Split('/').Last());
+                }
+                else
+                {
+                    actionMethod = clientType.GetMethod(actionName);
+                }
+                if (actionMethod == null)
+                {
+                    await this.Next.Invoke(context);
+                    return;
+                }
+
+                var parameters = new object[0];
+                var parameterInfos = actionMethod.GetParameters();
+                if (parameterInfos != null || parameterInfos.Count() > 0)
+                {
+                    if (request.ContentType.StartsWith("application/x-www-form-urlencoded", StringComparison.OrdinalIgnoreCase))
                     {
-                        var raw = await reader.ReadToEndAsync();
-                        parameters = new object[] { JsonConvert.DeserializeObject(raw, parameterInfos.First().ParameterType) };
+                        var form = await request.ReadFormAsync();
+                        parameters = ParseForm(form, parameterInfos);
+                    }
+                    else if (request.ContentType.StartsWith("application/json", StringComparison.OrdinalIgnoreCase))
+                    {
+                        using (var reader = new StreamReader(request.Body))
+                        {
+                            var raw = await reader.ReadToEndAsync();
+                            parameters = new object[] { JsonConvert.DeserializeObject(raw, parameterInfos.First().ParameterType) };
+                        }
                     }
                 }
+
+                var client = Activator.CreateInstance(clientType);
+                var result = actionMethod.Invoke(client, parameters);
+
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
             }
-
-            var client = Activator.CreateInstance(clientType);
-            var result = actionMethod.Invoke(client, parameters);
-
-            context.Response.ContentType = "application/json";
-            await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
 
         private object[] ParseForm(IFormCollection form, ParameterInfo[] parameterInfos)
